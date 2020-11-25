be6d3d8463a2c693112578cc81603eae9c92875e
flipflop133 <fbechet45@gmail.com>
2020-11-25 10:33:11 +0100

    initial commit

---
+++ weather.py
@@ -0,0 +1,84 @@
+import subprocess
+import json
+import requests
+import time
+
+error_time = 0
+
+
+def get_weather():
+    """Retrieve current weather from weatherapi.com
+    """
+    try:
+        # retrieve data from json file
+        with open("weather_settings.json", "r") as read_file:
+            data = json.load(read_file)
+        url = data['url']
+        key = data['key']
+        unit = "°C" if data['unit'] == "celcius" else "°F"
+        parameters = data['parameters']
+        daytime = {'sunset': data['sunset'], 'sunrise': data['sunrise']}
+
+        # retrieve weather from weatherapi.com
+        request = "{}?key={}&q={}".format(url, key, parameters)
+        response = requests.get(request)
+        data = json.loads(response.content)
+        conditions = data['current']['condition']['text']
+        temp = data['current']['temp_c']
+
+        # determine the icon
+        icon = get_icon(conditions, daytime)
+
+        # display weather
+        print("{} {}{}".format(icon, temp, unit))
+    except Exception as e:
+        print(e)
+        time.sleep(error_time)
+        error_time + 10
+        get_weather()
+
+
+def get_icon(conditions, daytime):
+    """ Determine weather icon in function of the current weather conditions
+    and hour.
+    """
+    icon = ''
+    hour = subprocess.run(['date', '+%H'],
+                          check=True,
+                          stdout=subprocess.PIPE,
+                          universal_newlines=True)
+    hour = (hour.stdout).strip()
+    sunset = daytime['sunset']
+    sunrise = daytime['sunrise']
+    # night icons
+    if int(hour) > sunset or int(hour) < sunrise:
+        if "cloud" in conditions:
+            icon = ''
+        elif "sun" in conditions:
+            icon = ''
+        elif "rain" in conditions:
+            icon = ''
+        elif "clear" in conditions:
+            icon = ''
+        else:
+            icon = ''
+    # day icons
+    else:
+        if "partly cloudy" in conditions:
+            icon = ''
+        elif "cloud" in conditions:
+            icon = ''
+        elif "sun" in conditions:
+            icon = ''
+        elif "rain" in conditions:
+            icon = ''
+        elif "clear" in conditions:
+            icon = ''
+        elif "fair" in conditions:
+            icon = ''
+        else:
+            icon = ''
+    return icon
+
+
+get_weather()
