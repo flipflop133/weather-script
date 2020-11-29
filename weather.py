80b434aafa6a0e172e803c822ae128c2112960b1
flipflop133 <fbechet45@gmail.com>
2020-11-29 13:28:32 +0100

    improve daily forecast display

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import time
+from datetime import date
 import os
 import datetime
 import argparse
@@ -139,36 +140,7 @@ def get_icon(conditions, daytime):
 
 
 def weather_app(forecast):
-    # display alternative weather informations
-    # init gtk window
-    win = Gtk.Window(title="Weather Forecast")
-    grid = Gtk.Grid()
-    # current date
-
-    vbox_infos_icon = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-    label = Gtk.Label(label="{}".format("Icon"))
-    label.set_markup("<span size='16000'>{}</span>".format("Icon"))
-    vbox_infos_icon.pack_start(label, True, True, 0)
-
-    vbox_infos_hour = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-    label = Gtk.Label(label="{}".format("Hour"))
-    label.set_markup("<span size='16000'>{}</span>".format("Hour"))
-
-    vbox_infos_hour.pack_start(label, True, True, 0)
-
-    vbox_infos_temp = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-    label = Gtk.Label(label="{}".format("Temp"))
-    label.set_markup("<span size='16000'>{}</span>".format("Temp"))
-
-    vbox_infos_temp.pack_start(label, True, True, 0)
-
-    vbox_infos = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-    vbox_infos.pack_start(label, True, True, 0)
-    vbox_infos.add(vbox_infos_hour)
-    vbox_infos.add(vbox_infos_icon)
-    vbox_infos.add(vbox_infos_temp)
-    grid.add(vbox_infos)
-
+    window = WeatherWindow()
     for hour in range(0, 24):
         icon = get_icon(
             forecast["forecast"]["forecastday"][0]["hour"][hour]["condition"]
@@ -176,6 +148,75 @@ def weather_app(forecast):
             forecast["forecast"]["forecastday"][0]["hour"][hour]["is_day"])
         temp = " {}°C  ".format(
             forecast["forecast"]["forecastday"][0]["hour"][hour]["temp_c"])
+        window.display(forecast, hour, icon, temp)
+    # launch window
+    window.connect("destroy", Gtk.main_quit)
+    window.show_all()
+    Gtk.main()
+
+
+class WeatherWindow(Gtk.Window):
+    main_grid = Gtk.Grid(orientation=Gtk.Orientation.VERTICAL)
+    morning_grid = Gtk.Grid(orientation=Gtk.Orientation.VERTICAL)
+    morning_grid_child = Gtk.Grid()
+    afternoon_grid = Gtk.Grid(orientation=Gtk.Orientation.VERTICAL)
+    afternoon_grid_child = Gtk.Grid()
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Weather Forecast")
+        self.main_grid.add(self.morning_grid)
+        self.main_grid.add(self.afternoon_grid)
+        self.add(self.main_grid)
+
+        for i in range(2):
+            # current date
+            vbox_infos_icon = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,
+                                      spacing=10)
+            label = Gtk.Label(label="{}".format("Icon"))
+            label.set_markup("<span size='16000'>{}</span>".format("Icon"))
+            vbox_infos_icon.pack_start(label, True, True, 0)
+
+            vbox_infos_hour = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,
+                                      spacing=10)
+            label = Gtk.Label(label="{}".format("Hour"))
+            label.set_markup("<span size='16000'>{}</span>".format("Hour"))
+
+            vbox_infos_hour.pack_start(label, True, True, 0)
+
+            vbox_infos_temp = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,
+                                      spacing=10)
+            label = Gtk.Label(label="{}".format("Temp"))
+            label.set_markup("<span size='16000'>{}</span>".format("Temp"))
+
+            vbox_infos_temp.pack_start(label, True, True, 0)
+
+            vbox_infos = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,
+                                 spacing=10)
+            vbox_infos.pack_start(label, True, True, 0)
+            vbox_infos.add(vbox_infos_hour)
+            vbox_infos.add(vbox_infos_icon)
+            vbox_infos.add(vbox_infos_temp)
+            if i == 0:
+                today = date.today()
+                app_title = Gtk.Label()
+                app_title.set_markup(
+                    "<span size='18000'>\n{} Forecast\n</span>".format(
+                        today.strftime("%B %d, %Y")))
+                title = Gtk.Label()
+                title.set_markup(
+                    "<span size='16000'>Morning Forecast\n</span>")
+                self.morning_grid.add(app_title)
+                self.morning_grid.add(title)
+                self.morning_grid_child.add(vbox_infos)
+            else:
+                title = Gtk.Label()
+                title.set_markup(
+                    "<span size='16000'>\nAfternoon Forecast\n</span>")
+                self.afternoon_grid.add(title)
+                self.afternoon_grid_child.add(vbox_infos)
+
+    def display(self, forecast, hour, icon, temp):
+
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
         label = Gtk.Label(label="{}h".format(str(hour + 1)))
         vbox.pack_start(label, True, True, 0)
@@ -196,13 +237,12 @@ def weather_app(forecast):
         vbox_final.set_size_request(70, 100)
         vbox_final.pack_start(label, True, True, 0)
 
-        grid.add(vbox_final)
-
-    # launch window
-    win.add(grid)
-    win.connect("destroy", Gtk.main_quit)
-    win.show_all()
-    Gtk.main()
+        if hour < 12:
+            self.morning_grid_child.add(vbox_final)
+            self.morning_grid.add(self.morning_grid_child)
+        else:
+            self.afternoon_grid_child.add(vbox_final)
+            self.afternoon_grid.add(self.afternoon_grid_child)
 
 
 get_weather(alternative)
